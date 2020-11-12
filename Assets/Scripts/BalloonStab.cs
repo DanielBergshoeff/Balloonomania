568d45acdb9c20fd5ff7ffd3da6e2dbddbc866d5
Arye <dabergshoeff@gmail.com>
2020-11-12 16:27:34 +0100

    Added player art functionality

---
+++ Assets/Scripts/BalloonStab.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class BalloonStab
+{
+    public BalloonInfo BalloonStabbing;
+    public BalloonInfo BalloonStabbed;
+    public Vector3 StabPosition;
+}
