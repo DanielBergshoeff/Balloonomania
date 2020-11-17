c82f86d60f2e811da0d281cb01a8c3b5dbfb7713
Arye <dabergshoeff@gmail.com>
2020-11-17 16:38:57 +0100

    Fixed AI prefab

---
+++ Assets/Scripts/StabEffect.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StabEffect : MonoBehaviour
+{
+    public GameObject ParticlePrefab;
+
+    private AudioSource myAudioSource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myAudioSource = gameObject.AddComponent<AudioSource>();
+    }
+
+    public void OnStab(BalloonStab balloonStab) {
+        myAudioSource.PlayOneShot(AudioManager.GetSound(Sound.BalloonPop));
+        GameObject go = Instantiate(ParticlePrefab);
+        go.transform.position = balloonStab.StabPosition;
+    }
+}
