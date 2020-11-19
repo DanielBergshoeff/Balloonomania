84a5e372d3517947d95821fb171173b9a70f6282
Shade Desktop <gamingkendoka@gmail.com>
2020-11-19 12:59:59 +0100

    Wip repeatables switcher

---
+++ Assets/Scripts/LizzyScripts/ChageRepeatables.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChageRepeatables : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter2D(Collider2D collider) //Once the player hits the end of the introduction level
+    {
+        Debug.Log("Entering trigger collider");
+        
+        //set LevelExtender's Background to regularLevelRepeatables
+    }
+}
---
+++ Assets/Scripts/LizzyScripts/LevelExtenderManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelExtenderManager : MonoBehaviour
+{
+    public LevelExtender currentLevelExtender;
+    public GameObject playerBalloon;
+    public GameObject regularLevelRepeatables;
+    public float xPosOffset = 3;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("LevelExtenderManager online!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(transform.position.x, playerBalloon.transform.position.y, transform.position.z);
+    }
+
+
+
+    private void OnTriggerEnter2D(Collider2D collider) //Once the player hits the end of the introduction level
+    {
+        Debug.Log("Entering trigger collider");
+        if (collider == playerBalloon){ 
+            Debug.Log("YEAAAAAAAAH!!!");
+        } 
+        //set LevelExtender's Background to regularLevelRepeatables
+    }
+}
